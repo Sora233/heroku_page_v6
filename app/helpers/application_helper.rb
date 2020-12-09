75cd54a8f461c082a589513326026f5cbd54a6b4
sora233 <sora@sora233.me>
2020-12-10 00:13:40 +0800

    [WIP] add navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,15 @@ module ApplicationHelper
     end
   end
 
+  def current_request?(*requests)
+    requests.each do |request|
+      if request[:controller].to_s == controller.controller_name.to_s
+        return true if request[:action].nil?
+        return true if request[:action].is_a?(Array) && request[:action].map(&:to_s).include?(controller.action_name.to_s)
+        return true if request[:action].to_s == controller.action_name.to_s || request[:action].to_s == '*'
+      end
+    end
+    false
+  end
+
 end
