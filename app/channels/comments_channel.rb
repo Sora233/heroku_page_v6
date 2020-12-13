d6935a13154d2d5c1a160fb76d9a53724011b78a
sora233 <sora@sora233.me>
2020-12-13 22:14:12 +0800

    fix comment channel button

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -7,11 +7,4 @@ class CommentsChannel < ApplicationCable::Channel
   def unsubscribe_all_post
     stop_all_streams
   end
-
-  def test(data)
-    post = Post.find(data["post_id"])
-    comment = Comment.first
-    renderer = ApplicationController.renderer_with_signed_in_user(current)
-    CommentsChannel.broadcast_to post, comment: renderer.render(partial: "comments/comment", locals: { comment: comment })
-  end
 end
--- app/jobs/comment_deliver_job.rb
+++ app/jobs/comment_deliver_job.rb
@@ -3,6 +3,6 @@ class CommentDeliverJob < ApplicationJob
 
   def perform(comment, user)
     renderer = ApplicationController.renderer_with_signed_in_user(user)
-    CommentsChannel.broadcast_to comment.commentable, comment: renderer.render(partial: "comments/comment", locals: { current_user: user, comment: comment })
+    CommentsChannel.broadcast_to comment.commentable, comment: renderer.render(partial: "comments/comment", locals: { current_user: user, comment: comment, skip_op_button: true })
   end
 end
